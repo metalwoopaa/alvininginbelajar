c2a7e77dbbb57283ffef6795cd3ca56904dcc0ab
pin <113010520+metalwoopaa@users.noreply.github.com>
2022-09-14 00:11:24 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,20 +2,29 @@
 <html>
 <head>
   <title>Login Form</title>
+  <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+<!-- <h2>Login Form</h2> -->
+
+<form action="tampil.php" method="POST">
+  <h2>Form Login </h2>
+  <div class="imgcontainer">
+    <img src="icon.png" alt="Avatar" class="avatar">
+  </div>
+
+  <div class="container">
+    <label><b>Username</b></label>
+    <input type="text" placeholder=Masukkan Username name="username" required>
+     <label for=""></label> 
+  </div>
+
+
+
+
+
+</form>
+
+</body>
 </html>
