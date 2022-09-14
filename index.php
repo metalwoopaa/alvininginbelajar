bdee3cb7ceee490da28efe2543ada685769e2003
pin <113010520+metalwoopaa@users.noreply.github.com>
2022-09-14 14:57:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Login Form</title>
+  <title>login form</title>
   <link rel="stylesheet" href="style.css" type="text/css">
-</head>
-<body>
-  
-<!-- <h2>Login Form</h2> -->
-
+  </head>
+  <body>
+    
+<!--  <h2>login form </h2>  -->
 <form action="tampil.php" method="POST">
   <h2>Form Login </h2>
   <div class="imgcontainer">
-    <img src="icon.png" alt="Avatar" class="avatar">
+    <img src="icon.png" alt="Avatar" class="avatar"
   </div>
-
+  
   <div class="container">
-    <label><b>Username</b></label>
-    <input type="text" placeholder=Masukkan Username name="username" required>
-     <label for=""></label> 
-  </div>
-
-
-
-
-
-</form>
-
-</body>
-</html>
+    <label><b>username</b></label>
+    <input type="text" placeholder="Masukan username" name="username" required>
+    
+    <label><b>password</b></label>
+    <input type="password" placeholder="masukkan password" name="email" required>
+    
+    <button type="submit">masuk</button>
+    <input type="checkbox" checked="checked"><span>ingat saya</span>
+    </div>
+    
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" class="cancelbtn">Batal</button>
+      <span class="psw">lupa  <a  href="#">password?</a></span>
+    </div>
+    </form>
+  
+  </body>
+  </html>
